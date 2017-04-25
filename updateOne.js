865646f33e88cda26afc32955f9e3d5335e6e5ca
yhl221 <1727538058@qq.com>
2017-04-25 16:13:12 +0800

    rename updataOne.js to updateOne.js

--- updateOne.js
+++ updateOne.js
@@ -3,7 +3,7 @@ const url = require("./helps/get-url");
 
 var update_one=(db,callback)=>{
     const collection=db.collection("users");
-    collection.updateOne({name:"Jack"},{$set:{job:"worker"}},(err,result)=>{
+    collection.updateOne({name:"Rose"},{$set:{age:"30"}},(err,result)=>{
         if(err){
             console.log("ERROR");
         }else{
