a5d8fdc18f3d21b31ea39bd88650de2d97a855d1
yhl221 <1727538058@qq.com>
2017-04-25 10:20:03 +0800

    add findAll.js

---
+++ findAll.js
@@ -0,0 +1,30 @@
+const MongoClient=require("./helps/mongodb");
+const url=require("./helps/get-url");  //数据库为second-sale
+
+
+const findAll=(db,callback)=>{
+    const collection=db.collection("users");
+    //查找 users 文档中的所有数据
+    collection.find({}).toArray((err,result)=>{
+        if(err){
+            console.log("ERROR");
+        }else{
+            console.log(" callback before");
+            callback(result);
+            console.log("callback after");
+        }
+    });
+};
+
+MongoClient.connect(url,(err,db)=>{
+    //连接到表 users
+    console.log("连接成功！");
+    findAll(db,(result)=>{
+      console.log(result.length);
+        for(var i=0;i<result.length;i++){
+            console.log(result[i]);
+        }
+        db.close();
+    });
+});
+
