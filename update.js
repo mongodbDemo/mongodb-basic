7fb6e568116baf70888ba48e80f2fab15f5befde
yhl221 <1727538058@qq.com>
2017-04-25 16:11:48 +0800

    modify update.js

--- update.js
+++ update.js
@@ -1,3 +1,23 @@
-/**
- * Created by yhl on 17-4-25.
- */
+const MongoClient = require("./helps/mongodb");
+const url = require("./helps/get-url");
+
+var update_data=(db,callback)=>{
+    const collection=db.collection("users");
+    collection.update({name:"Rose"},{$set:{job:"teacher"}},(err,result)=>{
+        if(err){
+            console.log("ERROR");
+        }else{
+            callback(result);
+        }
+
+        db.close()
+    });
+};
+
+
+MongoClient.connect(url,(err,db)=>{
+    console.log("连接成功");
+    update_data(db,(result)=>{
+        console.log(result);
+    });
+});
