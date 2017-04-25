eb2835320a4daae5400ede45e3292b795aaedcb9
yhl221 <1727538058@qq.com>
2017-04-25 11:11:54 +0800

    modify insert.js

--- insert.js
+++ insert.js
@@ -1,18 +1,26 @@
 const MongoClient=require("./helps/mongodb");
 const url=require("./helps/get-url");
 
-
-var find_and_modify = ( db, callback)=> {
+var insertData = (db, callback)=> {
     const collection=db.collection("users");
-    collection.findAndModify({name:"Bob"},{age:"30"},(result)=>{
+    //要插入的数据
+    const data=[{"name":"Look","age":"45"},
+               {"name":"Rose","age":"27"}];
+    collection.insert(data,(err,result)=>{
+        if(err){
+            console.log("ERROR");
+        }else{
+            callback(result);
+        }
 
+        db.close();
     });
 };
 
 
 MongoClient.connect(url,(err,db)=>{
-     console.log("连接成功！");
-    find_and_modify(,db,(result)=>{
-
-    });
-});
+    console.log("连接成功");
+        insertData(db,(result)=>{
+                console.log(result);
+            })
+        });
