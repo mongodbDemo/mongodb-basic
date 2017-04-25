5556e36d238469138b7a4302e69301bb3ede4dc5
yhl221 <1727538058@qq.com>
2017-04-25 15:29:07 +0800

    modify deleteOne.js

--- deleteOne.js
+++ deleteOne.js
@@ -1,3 +1,23 @@
-/**
- * Created by yhl on 17-4-25.
- */
+const MongoClient=require("./helps/mongodb");
+const url=require("./helps/get-url");
+
+var delete_one=(db,callback)=>{
+    const collection=db.collection("users");
+    collection.deleteOne({name:"Rose"},(err,result)=>{
+        if(err){
+            console.log("ERROR");
+        }else{
+            callback(result);
+        }
+
+        db.close();
+    })
+};
+
+
+MongoClient.connect(url, (err, db)=> {
+       console.log("连接成功！");
+       delete_one(db,(result)=>{
+           console.log(result);
+       });
+});
